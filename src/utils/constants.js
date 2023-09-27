5de7a0821852f661f4adcd500431f740c6dbc9b1
MainX342 <vladimirtitovperm@gmail.com>
2023-09-27 23:04:32 +0500

    fix: удалены неиспользуемые константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,5 @@
 const EmailRegex = "^\\S+@\\S+\\.\\S+$"
-const MediumScreen = 1024
-const SmallScreen = 480
-const InitLargeScreen = 12
-const InitMediumScreen = 8
-const InitSmallScreen = 5
-const StepLargeScreen = 4
-const StepMediumScreen = 3
-const StepSmallScreen = 2
 
 export {
-  EmailRegex,
-  MediumScreen,
-  SmallScreen,
-  InitLargeScreen,
-  InitMediumScreen,
-  InitSmallScreen,
-  StepLargeScreen,
-  StepMediumScreen,
-  StepSmallScreen
+  EmailRegex
 }
